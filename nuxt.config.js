7f2d517b89c340a1f0d0b985e2e348b9a312406d
zyxep <zyxep@zyxeps-iMac.local>
2018-09-16 17:26:45 +0200

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,51 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'fa5',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Add font awesome support
+   */
+  modules: [
+    'qonfucius-nuxt-fontawesome'
+  ],
+  fontAwesome: {
+    packs: [
+      {
+        package: '@fortawesome/fontawesome-free-brands',
+      }
+    ]
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
+
